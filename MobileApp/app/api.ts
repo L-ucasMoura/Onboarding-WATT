a5c0177a77cd588737871d2a034512053f018dc6
Lucas de Amorim Moura <lucas.amorimmoura@ufpe.br>
2025-10-01 22:05:37 -0300

    fix: comunicação app-server
    transmissão de dados entre o app Expo e o servidor python estabelecida

---
+++ MobileApp/app/api.ts
@@ -0,0 +1,20 @@
+
+export const enviarDado = async (x:Number, y:Number, z:Number) => {
+  try{
+    const response = await fetch("http://192.168.1.8:5000/enviar",{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({x, y, z}),
+    });
+
+    const data = await response.json();
+    console.log("Resposta do Servidor", data);
+    return data;
+  }
+  catch (error) {
+    console.error("Erro ao enviar dado", error);
+    throw error;
+  }
+};
--- MobileApp/app/index.tsx
+++ MobileApp/app/index.tsx
@@ -1,38 +1,26 @@
 import { Accelerometer } from "expo-sensors";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { enviarDado } from "./api";
 
 export default function Index() {
-    const [{x, y, z}, setData] = useState({x: 0, y: 0, z: 0});
+  const [{x, y, z}, setData] = useState({x: 0, y: 0, z: 0});
 
-    useEffect(() => {
-        const subscription = Accelerometer.addListener(setData);
-        return () => subscription.remove();
-    }, []);
+  useEffect(() => {
+      const subscription = Accelerometer.addListener((dados) => {
+        setData(dados);
+        enviarDado(dados.x, dados.y, dados.z);
+      });
+      return () => subscription.remove();
+  }, []);
 
-    Accelerometer.setUpdateInterval(500);
-
-    const radToDeg = (rad: number) => rad * (180 / Math.PI);
-
-  function getAngles(x: number, y: number, z: number) {
-    const pitch = radToDeg(Math.atan2(x, Math.sqrt(y * y + z * z)));
-    const roll = radToDeg(Math.atan2(y, Math.sqrt(x * x + z * z)));
-
-    return {
-      pitch: Number(pitch.toFixed(2)),
-      roll: Number(roll.toFixed(2)),
-    };
-  }
-
-  const { pitch, roll } = getAngles(x, y, z);
+  Accelerometer.setUpdateInterval(1000);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>X: {Number(radToDeg(x).toFixed(2))}</Text>
-      <Text>Y: {Number(radToDeg(y).toFixed(2))}</Text>
-      <Text>Z: {Number(radToDeg(z).toFixed(2))}</Text>
-      <Text>Pitch: {pitch}°</Text>
-      <Text>Roll: {roll}°</Text>
+      <Text>X: {x}</Text>
+      <Text>Y: {y}</Text>
+      <Text>Z: {z}</Text>
     </View>
   );
 }
